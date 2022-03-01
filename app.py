6eded86debd0a1a7e30a34625f83d6068d3591a7
ap23taka <ap23taka@gmail.com>
2022-03-01 13:25:25 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,82 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import mca
+from adjustText import adjust_text
+##
+st.title("Data to Pairplot APP")
+
+## Data
+st.subheader("Data Selection")
+uploaded_file = st.file_uploader(
+    label="Upload an excel file"
+)
+if uploaded_file is not None:
+    df = pd.read_excel(uploaded_file)
+    #
+    # fpath = "data/BrandValue.xlsx"
+    # df = pd.read_excel(fpath)
+
+    st.subheader("Uploaded Data")
+    st.write(df)
+
+    # correspondence analysis
+    df = df.set_index(df.columns[0])
+    ncols = df.shape[1]
+    mdl = mca.MCA(df, ncols=ncols, benzecri=False)
+    a1 = pd.DataFrame(mdl.fs_r(N=2), columns=["x", "y"]).assign(vartype="row", label= df.index)
+    a2 = pd.DataFrame(mdl.fs_c(N=2), columns=["x", "y"]).assign(vartype="col", label=df.columns)
+    dfp = pd.concat([a1, a2], ignore_index=True)
+
+    # scatter plot
+    st.subheader('Correspondence Mapping')
+    fig = sns.scatterplot(data=dfp, x="x", y="y", hue="vartype")
+    texts = [plt.text(dfp.x[i], dfp.y[i], dfp.label[i], ha='center', va='center') for i in range(len(dfp))]
+    adjust_text(texts)
+    # plt.show()
+    st.pyplot(fig)
+
+    # xy coordinate table
+    st.subheader("XY coordinates of Mapping")
+    st.write(dfp)
+
+    # Download
+    @st.cache
+    def convert_df(df):
+        # IMPORTANT: Cache the conversion to prevent computation on every rerun
+        return df.to_csv().encode('shift-JIS')
+
+    csv = convert_df(dfp)
+
+    st.download_button(
+        label="Download data as CSV",
+        data=csv,
+        file_name='MappingCoordinates.csv',
+        mime='text/csv',
+    )
+
+
+    # st.write(type(uploaded_file))
+    st.subheader("Uploaded Data")
+    st.write(df)
+    st.write(f"Columns : {df.columns.values}")
+
+    ### side bar
+    option = st.sidebar.multiselect(
+        label = 'Select columns for correlation plot.',
+        options=df.columns.values.tolist(),
+        default=df.columns.values[:4]
+    )
+
+    # plot
+    st.subheader('Pairplot (Matrix of Correlation plot)')
+    st.write("Select columns at side bar.")
+    st.write('Current selections :', option)
+
+    fig = sns.pairplot(data=df, vars=option, diag_kind="kde")
+    st.pyplot(fig)
+
+
+

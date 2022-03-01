3a1071003aeb50df52704e07a330560c50fd069a
ap23taka <ap23taka@gmail.com>
2022-03-01 13:58:46 +0900

    commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 import seaborn as sns
 import mca
 from adjustText import adjust_text
